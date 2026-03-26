49ec3ab5cb6841c0d93d41836c6919db7683f33c
Drăghici Andrei <draghici.andrei12@yahoo.com>
2026-03-26 06:52:47 +0200

    feat(os_runner): add test.py script to test the functionality of the new os_runner module

---
+++ test.py
@@ -0,0 +1,8 @@
+from os_runner import OsRunner
+
+
+if __name__ == '__main__':
+    my_os_runner = OsRunner()
+    my_os_runner.list_entries_in_path()
+    my_os_runner.change_current_directory('/')
+    my_os_runner.list_entries()
