f5c328ee75e702534ccc30d383e3761c0e769dfb
Drăghici Andrei <draghici.andrei12@yahoo.com>
2026-03-26 08:04:05 +0200

    chore(test): forgot to print, dummy

--- test.py
+++ test.py
@@ -3,6 +3,6 @@ from os_runner.os_runner import OsRunner
 
 if __name__ == '__main__':
     my_os_runner = OsRunner()
-    my_os_runner.list_entries_in_path()
+    print(f"Contents: {my_os_runner.list_entries_in_path()}")
     my_os_runner.change_current_directory('/')
-    my_os_runner.list_entries_in_path()
+    print(f"Contents: {my_os_runner.list_entries_in_path()}")
