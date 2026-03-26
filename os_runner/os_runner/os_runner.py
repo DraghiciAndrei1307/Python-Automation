1918526d482648bc2069d703eb981badda3d8ed9
Drăghici Andrei <draghici.andrei12@yahoo.com>
2026-03-26 06:50:01 +0200

    feat(os_runner): add setup.py and create OsRunner class with some basic methods

---
+++ os_runner/os_runner/os_runner.py
@@ -0,0 +1,32 @@
+import os
+
+class OsRunner:
+    def __init__(self):
+        pass
+
+
+    def list_entries_in_path(self, path=''):
+        """
+        This method lists all the entries in the given path.
+        Omit the path, and it will list the contents of the
+        current directory.
+        :param path:
+        :return:
+        """
+
+        return os.listdir(path)
+
+    def change_current_directory(self, path=''):
+
+        """
+        This method changes the current working directory.
+        :param path:
+        :return:
+        """
+
+        return os.chdir(path)
+
+    def run_cmd(self, command):
+        pass
+
+
---
+++ os_runner/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.0'
+DESCRIPTION = 'Performs os commands'
+LONG_DESCRIPTION = 'A python package for os commands'
+
+
+setup(
+    name='os_runner',
+    version=VERSION,
+    author='Andrei Draghici',
+    author_email='draghici.andrei12@yahoo.com',
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[ # here you will put your dependencies
+
+    ]
+)
+
