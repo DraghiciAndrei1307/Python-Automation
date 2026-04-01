300d0bf7db4c0d92ab2d23aed44e307c152cb724
Drăghici Andrei <draghici.andrei12@yahoo.com>
2026-04-01 17:14:46 +0300

    feat(os_executor): add os_executor package

---
+++ os_executor/setup.py
@@ -0,0 +1,25 @@
+"""
+    This is the setup.py script of the os_executor module.
+"""
+import click
+from setuptools import setup, find_packages
+
+VERSION = '0.1.0'
+DESCRIPTION = 'This is the CLI interface between the user and the os_runner'
+LONG_DESCRIPTION = ('This is a layer of abstraction that defines the way the '
+                    'user should use the os_runner capabilities.')
+
+
+setup(
+    name='os_executor_draghici_andrei',
+    version=VERSION,
+    author='Andrei Draghici',
+    author_email='draghici.andrei12@yahoo.com',
+    url='https://github.com/draghiciandrei1307/python-automation',
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[
+        click
+    ]
+)
