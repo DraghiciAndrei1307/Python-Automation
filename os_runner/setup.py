c0fa8a656eec72645f8c3c590801fe1beef5d935
Drăghici Andrei <draghici.andrei12@yahoo.com>
2026-04-01 10:34:26 +0300

    feat(ci): add new workflow named: publish.yml

--- os_runner/setup.py
+++ os_runner/setup.py
@@ -5,7 +5,7 @@
 
 from setuptools import setup, find_packages
 
-VERSION = '0.3.12'
+VERSION = '0.3.13'
 DESCRIPTION = 'Performs os commands'
 LONG_DESCRIPTION = 'A python package for os commands'
 
@@ -15,7 +15,7 @@ setup(
     version=VERSION,
     author='Andrei Draghici',
     author_email='draghici.andrei12@yahoo.com',
-    url='https://pypi.pkg.github.com/draghiciandrei1307/python-automation/',
+    url='https://github.com/draghiciandrei1307/python-automation',
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
