d7bceda65c6b866f6ddd1647bf74b6bf6c6cf51d
Drăghici Andrei <draghici.andrei12@yahoo.com>
2026-04-02 16:01:46 +0300

    feat(os_executor): add new commands(change_current_directory, create_text_file, change_mode, create_folder, move, run_cmd) into the os_executor

--- os_executor/os_executor/os_executor.py
+++ os_executor/os_executor/os_executor.py
@@ -17,7 +17,6 @@ def cli(ctx):
     """
     ctx.ensure_object(dict)
 
-
 @cli.command()
 @click.option('--count',
               default=1,
@@ -45,14 +44,110 @@ def goodbye(ctx, name):
     """Simple command that says goodbye NAME."""
     click.echo(f"Goodbye {name}!")
 
-
 @cli.command()
-@click.option('--path',
-              prompt='Enter a path',
-              help='The path you want to list the entries for.'
-              )
+@click.option(
+    '--path',
+    prompt='Enter a path',
+    help='The path you want to list the entries for.'
+)
 @click.pass_context
 def list_entries(ctx, path):
     os_runner = setup_os_runner()
     print(os_runner.list_entries_in_path(path))
 
+@cli.command()
+@click.option(
+    '--path',
+    prompt='Enter a path',
+    help='The path you want to go to.'
+)
+@click.pass_context
+def change_current_directory(ctx, path):
+    os_runner = setup_os_runner()
+    print(os_runner.change_current_directory(path))
+
+@cli.command()
+@click.option(
+    '--name',
+    prompt='How should the file be named?',
+    help='The name of the file you want to create.'
+)
+@click.option(
+    '--path',
+    prompt='Enter a path',
+    help='The path where you want to create the new file.'
+)
+@click.option(
+    '--mode',
+    prompt='Configure the mode of the file.',
+    help='The mode of the file. Use digits to configure the mode (e.g. 0644).'
+)
+@click.pass_context
+def create_text_file(ctx, name, path, mode):
+    os_runner = setup_os_runner()
+    print(os_runner.create_text_file(name, path, mode))
+
+@cli.command()
+@click.option(
+    '--path',
+    prompt='Enter a path',
+    help='The path you want to change the mode for.'
+)
+@click.option(
+    '--mode',
+    prompt='Mode',
+    help='The mode you want to change to.'
+)
+@click.pass_context
+def change_mode(ctx, path, mode):
+    os_runner = setup_os_runner()
+    print(os_runner.change_mode(path, mode))
+
+@cli.command()
+@click.option(
+    '--path',
+    prompt='Enter a path',
+    help='The path you want to create the new folder.'
+)
+@click.option(
+    '--name',
+    prompt='How should the folder be named?',
+    help='The name of the folder you want to create.'
+)
+@click.option(
+    '--mode',
+    prompt='Mode',
+    help='The mode you want to change to. '
+         'Use digits to configure the mode (e.g. 0644).'
+)
+@click.pass_context
+def create_folder(ctx, path, name, mode):
+    os_runner = setup_os_runner()
+    print(os_runner.create_folder(path, name, mode))
+
+@cli.command()
+@click.option(
+    '--src',
+    prompt='Enter a source path',
+    help='The source path you want to copy from.'
+)
+@click.option(
+    '--dest',
+    prompt='Enter a destination path',
+    help='The destination path you want to copy to.'
+)
+@click.pass_context
+def move(ctx, src, dest):
+    os_runner = setup_os_runner()
+    print(os_runner.move(src, dest))
+
+@cli.command()
+@click.option(
+    '--command',
+    prompt='Enter a command',
+    help='The command you want to execute.'
+)
+@click.pass_context
+def run_cmd(ctx, command):
+    os_runner = setup_os_runner()
+    print(os_runner.run_cmd(command))
