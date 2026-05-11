e54abfd48675443abac0d61fe67dd1f7beae7268
Drăghici Andrei <draghici.andrei12@yahoo.com>
2026-05-11 21:58:16 +0300

    feat(django_rest_api): add a new package named django_rest_api

--- django_rest_api/api_config/settings.py
+++ django_rest_api/api_config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y#%fmpwm4wu0cv*@k-i66smj)3raetu$nfm)1tsdilq80_ya)2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
