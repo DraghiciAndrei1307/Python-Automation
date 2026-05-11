8602325114018df548951a9743e98a7d161903b4
Drăghici Andrei <draghici.andrei12@yahoo.com>
2026-05-11 21:43:46 +0300

    feat(django_rest_api): add a new package named django_rest_api

--- django_rest_api/api_config/urls.py
+++ django_rest_api/api_config/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.urls import include, path
 from rest_framework import routers
 
-from django_rest_api.provisioner_api import views
+from provisioner_api import views
 
 router = routers.DefaultRouter()
 router.register(r"users", views.UserViewSet)
