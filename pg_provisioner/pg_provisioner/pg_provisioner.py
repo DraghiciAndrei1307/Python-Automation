5c8f9a1d63f505e5e461abd91dd6fbcd245f749c
Drăghici Andrei <draghici.andrei12@yahoo.com>
2026-05-13 18:55:07 +0300

    fix(pg_provisioner): add absolute path to the .vault_pass file

--- pg_provisioner/pg_provisioner/pg_provisioner.py
+++ pg_provisioner/pg_provisioner/pg_provisioner.py
@@ -49,7 +49,7 @@ class PgProvisioner:
     def start_pg_vm_provisioning(self):
 
         result = self.os_runner.run_cmd(
-            input_command='ansible-playbook -i /home/student/PostgreSQL-Ansible-Automation/ansible/inventories/ /home/student/PostgreSQL-Ansible-Automation/ansible/provision_postgresql_VM.yml',
+            input_command='ansible-playbook -i /home/student/PostgreSQL-Ansible-Automation/ansible/inventories/ /home/student/PostgreSQL-Ansible-Automation/ansible/provision_postgresql_VM.yml --vault-password-file /home/student/.vault_pass',
             input_data=f"{self.vault_password}\n"
         )
 
